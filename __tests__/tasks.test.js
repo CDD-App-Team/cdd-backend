03ad1d71cb754a49de7ee3409a0026106e971bc3
KASHI TAMANG <tamang.kashi@gmail.com>
2022-10-06 12:13:45 -0700

    allows POST to tasks for authenticated user

--- __tests__/tasks.test.js
+++ __tests__/tasks.test.js
@@ -6,23 +6,23 @@ const UserService = require('../lib/services/UserService');
 const Task = require('../lib/models/Task');
 
 const mockUser = {
-  email: 'test4@example.com',
-  password: '123456',
-};
-
-const mockUser2 = {
   firstName: 'Test',
-  lastName: 'User 2',
-  email: 'test2@example.com',
-  password: '123456',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
 };
 
 const registerAndLogin = async (userProps = {}) => {
   const password = userProps.password ?? mockUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
   const agent = request.agent(app);
+
+  // Create a user to sign in with
   const user = await UserService.create({ ...mockUser, ...userProps });
 
-  //sign in
+  // ...then sign in
   const { email } = user;
   await agent.post('/api/v1/users/sessions').send({ email, password });
   return [agent, user];
